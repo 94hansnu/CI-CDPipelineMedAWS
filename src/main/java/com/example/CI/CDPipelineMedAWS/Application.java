0904952440ac569d0bc003cbbe1ae91a23734d74
94hansnu <94hansnu@gafe.molndal.se>
2024-02-08 13:28:45 +0100

    implementerat enklare menu

--- src/main/java/com/example/CI/CDPipelineMedAWS/Application.java
+++ src/main/java/com/example/CI/CDPipelineMedAWS/Application.java
@@ -1,13 +1,18 @@
 package com.example.CI.CDPipelineMedAWS;
 
+import com.example.CI.CDPipelineMedAWS.client.Menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+		Menu menu = context.getBean(Menu.class);
+		menu.displayMenu();
 	}
 
 }
